e70dfbd8ee8168a7a26a60150c7220f01577c065
indraantoor <indraantoor@gmail.com>
2022-07-07 01:00:48 +0530

    style: setup color palette css variables
    Signed-off-by: indraantoor <indraantoor@gmail.com>

--- src/App.style.js
+++ src/App.style.js
@@ -2,6 +2,17 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   :root {
-    
+    --main-bg: #EEF4ED;
+    --primary: #134074;
+    --secondary: #13315C;
+    --primary-accent: #0B2545;
+    --secondary-accent: #8DA9C4; 
+    --primary-white: #EEF4ED;
+    --primary-black: rgb(41, 41, 41);
+  }
+
+  body {
+    background-color: var(--main-bg);
+    color: var(--primary-black);
   }
 `;
