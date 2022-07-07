e716519b0fcba305f1aac1a7a09badaeedb2a6a6
indraantoor <indraantoor@gmail.com>
2022-07-07 06:03:16 +0530

    add: setup react router with home and view profile route
    Signed-off-by: indraantoor <indraantoor@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from "react";
 import { GlobalStyle } from "./App.style";
 import { Home } from "./pages/Home";
 import { MainPageLayout } from "./pages/shared/MainPageLayout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Profile } from "./pages/Profile";
 
 const App = () => {
   return (
-    <React.Fragment>
-      <GlobalStyle />
-      <MainPageLayout>
-        <Home />
-      </MainPageLayout>
-    </React.Fragment>
+    <Router>
+      <React.Fragment>
+        <GlobalStyle />
+        <MainPageLayout>
+          <Routes>
+            <Route path="/profile/:id" element={<Profile />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </MainPageLayout>
+      </React.Fragment>
+    </Router>
   );
 };
 
