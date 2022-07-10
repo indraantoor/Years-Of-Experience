963fee8b3bf3ced74dea9ceff2328a0562004ed6
indraantoor <indraantoor@gmail.com>
2022-07-11 03:09:35 +0530

    feat: page works in offline mode
    Signed-off-by: indraantoor <indraantoor@gmail.com>

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,24 @@
-console.warn("ws fkle in public");
+let cacheData = "appV1";
+
+this.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheData).then((cache) => {
+      cache.addAll([
+        "/static/js/bundle.js",
+        "/index.html",
+        "/",
+        "/profile/indraantoor",
+      ]);
+    })
+  );
+});
+
+this.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => {
+      if (res) {
+        return res;
+      }
+    })
+  );
+});
