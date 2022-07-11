03dc968df940bfb7507ff3114e68e38a3a020581
indraantoor <indraantoor@gmail.com>
2022-07-11 13:46:03 +0530

    update: service worker is enabled for all environments
    Signed-off-by: indraantoor <indraantoor@gmail.com>

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import { store } from "./store";
 import { Provider } from "react-redux";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import { swDev } from "./swDev";
+// import { swDev } from "./swDev";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
