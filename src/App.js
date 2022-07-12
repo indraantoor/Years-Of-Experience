df5097eefb8aba62fb354a8e4dac25bf11ab6b8f
indraantoor <indraantoor@gmail.com>
2022-07-12 15:31:14 +0530

    refactor(App.js): removed comments and unused imports
    Signed-off-by: indraantoor <indraantoor@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { GlobalStyle } from "./App.style";
 import { Home } from "./pages/Home";
 import { MainPageLayout } from "./pages/shared/MainPageLayout";
@@ -8,26 +8,8 @@ import { PageNotFound } from "./pages/shared/PageNotFound";
 import { EditProfile } from "./pages/EditProfile";
 import { BasicDetailsEdit } from "./pages/BasicDetailsEdit";
 import { WorkExperiencesEdit } from "./pages/WorkExperiencesEdit";
-import { fetchUserDetailsFromApi } from "./store/userDetailsSlice";
-import { useDispatch } from "react-redux";
-import { onSnapshot, collection } from "@firebase/firestore";
-import { db } from "./firebase-config";
 
 const App = () => {
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   const userId = "V4QsOhuPZXQHXJ2Dw8QI";
-  //   dispatch(fetchUserDetailsFromApi(userId));
-  // }, []);
-
-  // const unsub = onSnapshot(collection(db, "users"), (snapShot) => {
-  //   snapShot.docs.forEach((doc) => {
-  //     console.log({ id: doc.id, ...doc.data() });
-  //     console.log(doc);
-  //   });
-  // });
-
   return (
     <Router>
       <React.Fragment>
