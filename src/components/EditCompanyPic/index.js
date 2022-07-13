2979967a54bfbd37157555d672282d495d891f4a
indraantoor <indraantoor@gmail.com>
2022-07-14 02:37:00 +0530

    refactor(edit-company-pic): refactor upload function to helper function
    Signed-off-by: indraantoor <indraantoor@gmail.com>

--- src/components/EditCompanyPic/index.js
+++ src/components/EditCompanyPic/index.js
@@ -10,17 +10,17 @@ import { updateWorkExperienceToApi } from "../../store/workExperiencesSlice";
 export const EditCompanyPic = () => {
   const [file, setFile] = useState(null);
   const [isError, setIsError] = useState(false);
+
+  const dispatch = useDispatch();
+
   const params = useParams();
 
+  const userId = useSelector((state) => state.userDetails.id);
   const workExperience = useSelector((state) =>
     state.workExperiences.data.find(
       (experience) => experience.id == params.workExperienceId
     )
   );
-
-  const userId = useSelector((state) => state.userDetails.id);
-
-  const dispatch = useDispatch();
 
   const handleImageChange = (event) => {
     if (event.target.files[0]) {
@@ -30,52 +30,8 @@ export const EditCompanyPic = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const fileName = new Date().getTime() + file.name;
-    const storageRef = ref(storage, fileName);
-    const uploadTask = uploadBytesResumable(storageRef, file);
-
-    uploadTask.on(
-      "state_changed",
-      (snapshot) => {
-        // Observe state change events such as progress, pause, and resume
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        setIsError(false);
-        const progress =
-          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log("Upload is " + progress + "% done");
-        switch (snapshot.state) {
-          case "paused":
-            console.log("Upload is paused");
-            break;
-          case "running":
-            console.log("Upload is running");
-            break;
-          default:
-        }
-      },
-      (error) => {
-        // Handle unsuccessful uploads
-        setIsError(true);
-      },
-      () => {
-        // Handle successful uploads on complete
-        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          setIsError(false);
-          const updatedDetails = { companyLogo: downloadURL };
-          const details = {
-            ...updatedDetails,
-            userId: userId,
-            id: params.workExperienceId,
-          };
-
-          dispatch(workExperiencesSlice.actions.update(details));
-          dispatch(updateWorkExperienceToApi(details));
-
-          console.log("Download URL", updatedDetails);
-        });
-      }
-    );
+    const { workExperienceId } = params;
+    uploadPic(file, setIsError, userId, workExperienceId, dispatch);
   };
 
   return (
@@ -94,3 +50,55 @@ export const EditCompanyPic = () => {
     </Container>
   );
 };
+
+/*
+   Helper Function
+*/
+
+function uploadPic(file, setErrFn, userId, workExperienceId, dispatch) {
+  const fileName = new Date().getTime() + file.name;
+  const storageRef = ref(storage, fileName);
+  const uploadTask = uploadBytesResumable(storageRef, file);
+
+  uploadTask.on(
+    "state_changed",
+    (snapshot) => {
+      // Observe state change events such as progress, pause, and resume
+      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      setErrFn(false);
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log("Upload is " + progress + "% done");
+      switch (snapshot.state) {
+        case "paused":
+          console.log("Upload is paused");
+          break;
+        case "running":
+          console.log("Upload is running");
+          break;
+        default:
+      }
+    },
+    (error) => {
+      // Handle unsuccessful uploads
+      setErrFn(true);
+    },
+    () => {
+      // Handle successful uploads on complete
+      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        setErrFn(false);
+        const updatedDetails = { companyLogo: downloadURL };
+        const details = {
+          ...updatedDetails,
+          userId: userId,
+          id: workExperienceId,
+        };
+
+        dispatch(workExperiencesSlice.actions.update(details));
+        dispatch(updateWorkExperienceToApi(details));
+
+        console.log("Download URL", updatedDetails);
+      });
+    }
+  );
+}
